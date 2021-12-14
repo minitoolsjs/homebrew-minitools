d1996eb5dd8910cf94a445b7189ed2ca64b3845e
张朝杰 <zhangchaojie@bytedance.com>
2021-12-14 11:59:12 +0800

    commit

---
+++ Formula/minitools.rb
@@ -0,0 +1,16 @@
+class MiniTools < Formula
+  desc "miniTools"
+  homepage "https://minitoolsjs.github.io"
+  url "https://github.com/minitoolsjs/minitools.git", branch: "main"
+  version "0.0.1"
+  license ""
+
+  def install
+    system "make", "build"
+    bin.install Dir["bin/*"]
+  end
+
+  test do
+    system "false"
+  end
+end
